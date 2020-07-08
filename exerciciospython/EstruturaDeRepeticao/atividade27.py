5ba34f3f262bdabb300797e632e17fc82dff7456
Frederico Gustavo Magalhães <fredlgprime@gmail.com>
2020-07-08 01:44:02 -0300

    Novo exercício
    Exercicio 27

--- exerciciospython/EstruturaDeRepeticao/atividade27.py
+++ exerciciospython/EstruturaDeRepeticao/atividade27.py
@@ -2,3 +2,18 @@
 27 - Faça um programa que calcule o número médio de alunos por turma. Para isto, peça a quantidade de turmas e a
 quantidade de alunos para cada turma. As turmas não podem ter mais de 40 alunos.
 '''
+turmas = int(input('Quantas turmas? '))
+alunos_turmas = []
+turma = 1
+
+for i in range(turmas):
+    print('turma ', turma)
+    alunos = int(input('Alunos da turma : '))
+    while alunos > 40:
+        print(f'turma {turma} [uma turma só pode ter 40 alunos]')
+        alunos = int(input('Alunos da turma : '))
+    turma += 1
+    alunos_turmas.append(alunos)
+
+media = sum(alunos_turmas) / len(alunos_turmas)
+print(f'A media é igual a: {media}')
